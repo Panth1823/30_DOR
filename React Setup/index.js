50b7ecac2ec9fa7639321c74146f4cd232d428d3
Panth Shah <89696311+Panth1823@users.noreply.github.com>
2023-06-08 22:28:20 +0530

    react setup

---
+++ React Setup/index.js
@@ -0,0 +1,2 @@
+ReactDOM.render(<h1>Hello, everyone ⚡</h1>, document.getElementById("root"))
+ReactDOM.render(<ul><li>blue</li><li>red</li><li>pink</li><li>green</li></ul>, document.getElementById("play"))
